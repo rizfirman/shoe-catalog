a66802a2080e3c32e09e57f1f174e6da84d8dbe6
Rizki Firmansyah <rizfirman@gmail.com>
2024-11-15 13:33:41 +0700

    remove unused package

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: "2024-04-03",
+  compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
-  css: ["@/assets/scss/global.scss"],
-  modules: [
-    "@pinia/nuxt",
-    "@nuxt/eslint",
-    "nuxt-svgo",
-    "@nuxt/image",
-  ],
-});
+  css: ['@/assets/scss/global.scss'],
+  modules: ['@pinia/nuxt', '@nuxt/eslint'],
+})
